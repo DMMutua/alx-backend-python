4ed9b50a64011b1bfe4bbd506a3edcf5af52cb21
DMMutua <lengowriter99@gmail.com>
2023-05-02 11:45:41 +0300

    return random waittime after waiting for it

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """Waits for a random delay between 0 and max_delay
+    and returns it."""
+
+    num = random.uniform(0, max_delay)
+    await asyncio.sleep(num)
+
+    return num
