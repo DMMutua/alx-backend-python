f8cc82d8d32f7784c322fb5e1f9b110869be46b3
DMMutua <lengowriter99@gmail.com>
2023-05-02 12:26:35 +0300

    using `create_tasks` and `as_completed` methods to log out list of sorted delay times

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """ spawn `wait_random` `n` times with the specified `max_delay`.
+    Return the list of all the delays (float values).
+    list of the delays should be in ascending order without using
+    sort() because of concurrency.
+    """
+
+    tasks = []
+    for i in range(n):
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
