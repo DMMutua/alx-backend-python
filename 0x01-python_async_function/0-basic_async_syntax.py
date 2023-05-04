2afa54f92efb4feff6e8023c551a78cedfb39dd3
DMMutua <lengowriter99@gmail.com>
2023-05-04 19:27:32 +0300

    annotated function with a param with default value

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""Module Containing a """
+"""Module Containing a Function that tests basic
+Async Syntax"""
 
 import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> int:
     """Waits for a random delay between 0 and max_delay
     and returns it."""
 
